386785d2691fe604586622f2d6789f6deb8ab7dc
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2026-07-18 19:58:27 +0330

    convert js to jsx

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import MainHeader from "@/components/mainHeader/MainHeader";
+
+import "./globals.css";
+
+export const metadata = {
+	title: "Next.js Page Routing & Rendering",
+	description: "Learn how to route to different pages.",
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body>
+				<div id="page">
+					<MainHeader />
+					{children}
+				</div>
+			</body>
+		</html>
+	);
+}
