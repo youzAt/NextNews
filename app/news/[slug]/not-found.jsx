d690326479075b0f882072d6fac618b4618e9416
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2026-07-18 19:59:16 +0330

    add news pages and components for displaying articles and handling not found cases

---
+++ app/news/[slug]/not-found.jsx
@@ -0,0 +1,10 @@
+const NewsNotFoundPage = () => {
+	return (
+		<div id="error">
+			<h1>Not Found!</h1>
+			<p>Unfotunately, we could not find the reqeusted article!</p>
+		</div>
+	);
+};
+
+export default NewsNotFoundPage;
---
+++ app/news/[slug]/page.jsx
@@ -0,0 +1,26 @@
+import { notFound } from "next/navigation";
+
+import { DUMMY_NEWS } from "@/dummy-news";
+
+const NewsDetailPage = async ({ params }) => {
+	const { slug: newsSlug } = await params;
+	const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+
+	if (!newsItem) notFound();
+
+	return (
+		<article className="news-article">
+			<header>
+				<img
+					src={`/images/news/${newsItem.image}`}
+					alt={newsItem.title}
+				/>
+				<h1>{newsItem.title}</h1>
+				<time dateTime={newsItem.date}>{newsItem.date}</time>
+			</header>
+			<p>{newsItem.content}</p>
+		</article>
+	);
+};
+
+export default NewsDetailPage;
